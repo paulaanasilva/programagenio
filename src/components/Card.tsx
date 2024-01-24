bf98c2273e95b893e5058c1e48d5912d9eb087a1
ana.silva <ana.silva@senior.com.br>
2024-01-24 17:57:35 -0300

    Ajuste no dropdown

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -21,6 +21,7 @@
 }
 import React from "react";
 import Image from "next/image";
+import DropdownCurso from "./DropdownCurso";
 
 interface CardProps {
   img: string;
@@ -31,64 +32,25 @@ interface CardProps {
 function Card(props: CardProps) {
   return (
     <>
-    <div className="border-collapse border border-slate-400 rounded-lg my-3 p-3">
-      <div className="flex">
-        <Image
-          className="imgCurso rounded-lg m-1"
-          src={props.img}
-          width={300}
-          height={200}
-          alt="Imagem de identificação do curso"
-        />
-        <div className="m-1">
-          <h3>{props.title}</h3>
-          <br />
-          <span>{props.description}</span>
+      <div className="border-collapse border border-slate-400 rounded-lg my-3 p-3 box">
+        <div>
+          <Image
+            className="imgCurso rounded-lg m-1"
+            src={props.img}
+            width={300}
+            height={200}
+            alt="Imagem de identificação do curso" />
         </div>
-      </div>
-      <div className="flex justify-end">
-        <button id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots" className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
-          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
-            <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
-          </svg>
-        </button>
-
-        {/* Dropdown menu */}
-        <div id="dropdownDots" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-          <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
-            <li>
-              <a className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-            </li>
-            <li>
-              <a className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-            </li>
-            <li>
-              <a className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-            </li>
-          </ul>
+        <div>
+          <div className="m-1">
+            <h3>{props.title}</h3>
+            <span>{props.description}</span>
+          </div>
         </div>
-      </div>
-    </div>
-    <div className="flex justify-end">
-        <button id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots" className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button">
-          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
-            <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
-          </svg>
-        </button>
-
-        {/* Dropdown menu */}
-        <div id="dropdownDots" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-          <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
-            <li>
-              <a className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-            </li>
-            <li>
-              <a className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-            </li>
-            <li>
-              <a className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-            </li>
-          </ul>
+        <div className="push">
+        </div>
+        <div>
+          <DropdownCurso />
         </div>
       </div>
     </>
